3886350067a61cfa5ccf94daff4ed348f7cf90d1
erguotou <erguotou525@gmail.com>
2017-11-16 22:09:59 +0800

    tt

--- src/components/business/CircleBg.vue
+++ src/components/business/CircleBg.vue
@@ -1,7 +1,7 @@
 <template>
-  <svg viewBox="0 0 1366 400">
+  <svg viewBox="0 0 1366 500">
     <!-- M0,300H1366V49.9C1184.41,18.86,945.17,0,683,0S181.59,18.86,0,49.9Z -->
-    <path :class="$style.primary" d="M0 0 V400 H1366 V0 H0Z"/>
+    <path :class="$style.primary" d="M0,0 L1366,0 L1366,300 C1366,300 1064.00696,500 683,500 C301.993042,500 0,300 0,300 L0,0 Z"/>
   </svg>
 </template>
 <script>
@@ -13,9 +13,4 @@ export default {
 @import '../../assets/styles/variable.styl'
 .primary
   fill $color-primary
-</style>
-<style lang="stylus" scoped>
-@import '../../assets/styles/variable.styl'
-svg
-  box-shadow 0 10px 40px 2px rgba($color-primary, .5)
 </style>
